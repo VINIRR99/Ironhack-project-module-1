349d14a7ac2db9da2a8be5f3e22a243f5a38b409
Vinicius Leite Batista <viniciusleite384@gmail.com>
2021-12-14 21:28:57 -0400

    Project part 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,18 @@
 const canvas = document.getElementById('project');
 const ctx = canvas.getContext('2d');
 
-ctx.beginPath();
-ctx.moveTo(638, 0);
-ctx.lineTo(638, 600);
-ctx.lineWidth = 4;
-ctx.setLineDash([5, 3]);
-ctx.strokeStyle = "white";
-ctx.stroke();
-ctx.closePath();
+const drawNet = () => {
+    ctx.beginPath();
+    ctx.moveTo(638, 0);
+    ctx.lineTo(638, 600);
+    ctx.lineWidth = 4;
+    ctx.setLineDash([5, 3]);
+    ctx.strokeStyle = "white";
+    ctx.stroke();
+    ctx.closePath();
+};
+
+drawNet();
 
 class Rectangle {
     constructor(positionX, positionY, width, height) {
@@ -19,7 +23,7 @@ class Rectangle {
         this.speedY = 0;
     };
 
-    draw = () => {
+    draw() {
         ctx.fillStyle = "white";
         ctx.fillRect(this.positionX, this.positionY, this.width, this.height);
     };
@@ -30,18 +34,22 @@ class Player extends Rectangle {
         super(positionX, 265, 20, 70, 0);
     };
 
-    moveUp = () => {
-        ctx.clearRect(this.positionX, this.positionY, this.width, this.height);
-        this.speedY = 8;
+    moveUp() {
+        if (this.positionY > 0) {
+            this.speedY = 8;
+        } else {
+            this.speedY = 0;
+        };
         this.positionY -= this.speedY;
-        this.draw();
     };
 
-    moveDown = () => {
-        ctx.clearRect(this.positionX, this.positionY, this.width, this.height);
-        this.speedY = 8;
+    moveDown() {
+        if (this.positionY < (canvas.height - this.height)) {
+            this.speedY = 8;
+        } else {
+            this.speedY = 0;
+        };
         this.positionY += this.speedY;
-        this.draw();
     };
 };
 
@@ -66,16 +74,51 @@ window.addEventListener("load", () => {
             case "ArrowDown":
                 player2.moveDown();
         };
+        updateCanvas();
     });
 });
+/*
+function degreesToRadians(degree) {
+    return (Math. PI / 180) * degree;
+}; */
 
 class Ball extends Rectangle {
     constructor() {
         super(615, 290, 20, 20, 0);
         this.speedX = 0;
+        this.angle = 180;
+    };
+
+    newPosition() {
+        if ((this.positionY + this.speedY) < (canvas.height - this.height) || (this.positionY + this.speedY) > 0) {
+            this.speedY = -2;
+        } else {
+            this.speedY = 2;
+        };
+        this.speedX = 2;
+
+        this.positionX += this.speedX;
+        this.positionY += this.speedY;
+    };
+
+    move() {
+        const intervalId = setInterval(() => {
+            updateCanvas();
+            this.newPosition()
+        }, 10);
     };
 };
 
 const ball = new Ball();
 
 ball.draw();
+ball.move();
+
+function updateCanvas() {
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+
+    drawNet();
+    player1.draw();
+    player2.draw();
+    ball.draw();
+};
